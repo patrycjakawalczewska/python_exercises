9b45aaf6c93f5ec5602993cb8c9eeae31b7cbd2f
Patrycja Kawalczewska <90581454+patrycjakawalczewska@users.noreply.github.com>
2023-05-18 01:49:47 +0200

    Update even_characters.py

--- even_characters.py
+++ even_characters.py
@@ -3,3 +3,23 @@
 #Write a program to accept a string from the user and display characters that are present at an even index number.
 #For example, str = "pynative" so you should display ‘p’, ‘n’, ‘t’, ‘v’.
 
+#1) option:
+
+print('Orginal String is  pynative')
+print('Printing only even index chars')
+str = "pynative"
+
+#get even letters:
+even = str[::2]
+#create an iteration
+iteration = iter(even)
+
+#print each of the even letters:
+print(next(iteration))
+print(next(iteration))
+print(next(iteration))
+print(next(iteration))
+
+#2) option:
+
+
