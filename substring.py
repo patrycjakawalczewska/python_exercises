3d5041849931467864eb00edbe3c23497d923ee4
Patrycja Kawalczewska <90581454+patrycjakawalczewska@users.noreply.github.com>
2023-05-20 08:45:00 +0200

    Create substring.py

---
+++ substring.py
@@ -0,0 +1,7 @@
+##Exercise 7: Return the count of a given substring from a string
+#Write a program to find how many times substring “Emma” appears in the given string.
+
+#given:
+str_x = "Emma is good developer. Emma is a writer"
+
+print('Emma appeared', str_x.count('Emma'), 'times')
