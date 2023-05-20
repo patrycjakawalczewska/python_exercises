b129f60c0a493263a842f96f15aa793f963f108c
Patrycja Kawalczewska <90581454+patrycjakawalczewska@users.noreply.github.com>
2023-05-20 08:33:07 +0200

    Update iterator.divide.py

--- iterator.divide.py
+++ iterator.divide.py
@@ -1,3 +1,10 @@
 ##Exercise 6: Display numbers divisible by 5 from a list
 #Iterate the given list of numbers and print only those numbers which are divisible by 5
 
+list = [10, 20, 33, 46, 55]
+print('Given list is [10, 20, 33, 46, 55]')
+print('Divisible by 5:')
+for x in list:
+# % is also called 'modulo' and means 'the remainder'
+    if x % 5 == 0:
+        print(x)
