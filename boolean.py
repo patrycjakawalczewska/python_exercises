0fc620f915b91040f198152dbe09d3c39be824af
Patrycja Kawalczewska <90581454+patrycjakawalczewska@users.noreply.github.com>
2023-05-20 07:54:26 +0200

    Update boolean.py

--- boolean.py
+++ boolean.py
@@ -3,6 +3,23 @@
 #If numbers are different then return False.
 
 #Given:
-#numbers_x = [10, 20, 30, 40, 10]
-#numbers_y = [75, 65, 35, 75, 30]
+#numbers_1 = [10, 20, 30, 40, 10]
+
+numbers_1 = [10, 20, 30, 40, 10]
+def list(numbers_1):
+    if numbers_1[0] == numbers_1[-1]:
+        return True
+    else:
+        return False
+print("result is", list(numbers_1))
+
+#numbers_2 = [75, 65, 35, 75, 40]
+
+numbers_2 = [75, 65, 35, 75, 40]
+def list(numbers_2):
+    if numbers_2[0] == numbers_2[-1]:
+        return True
+    else:
+        return False
+print("result is", list(numbers_2))
 
