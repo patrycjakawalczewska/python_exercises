3ceb5bed0b083f39d60bcc30a1910ef3b9e4290b
Patrycja Kawalczewska <90581454+patrycjakawalczewska@users.noreply.github.com>
2023-05-20 17:40:58 +0200

    Update pattern.py

--- pattern.py
+++ pattern.py
@@ -6,3 +6,30 @@
 #4 4 4 4 
 #5 5 5 5 5
 
+def print_pattern(x):
+    for y in range(1, x+1):
+        print('#', end='')
+        for z in range(1, y+1):
+            print(str(y), end =" ")
+# new line after each row to display pattern correctly:
+        print()
+print_pattern(5)
+
+#without hashtags:
+
+def print_pattern(x):
+    for y in range(1, x+1):
+        for z in range(1, y+1):
+            print(str(y), end =" ")
+# new line after each row to display pattern correctly:
+        print()
+print_pattern(5)
+
+#2nd option:
+
+
+for num in range(10):
+    for i in range(num):
+        print(num, end=" ")
+    # new line after each row to display pattern correctly
+     print()
