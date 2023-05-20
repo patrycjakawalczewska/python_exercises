b94090ce8e6b017d79a21cf770ba9161daf426de
Patrycja Kawalczewska <90581454+patrycjakawalczewska@users.noreply.github.com>
2023-05-20 21:14:00 +0200

    Create list_from_another_list.py

---
+++ list_from_another_list.py
@@ -0,0 +1,9 @@
+##Exercise 10: Create a new list from a two list using the following condition
+
+#Create a new list from a two list using the following condition:
+#Given a two list of numbers, write a program to create a new list such that the new list should 
+#contain odd numbers from the first list and even numbers from the second list.
+
+#Given:
+list1 = [10, 20, 25, 30, 35]
+list2 = [40, 45, 60, 75, 90]
