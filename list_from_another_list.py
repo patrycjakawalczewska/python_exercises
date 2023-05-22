eab0ea10a84464fe327d048c080bb8749da0d888
Patrycja Kawalczewska <90581454+patrycjakawalczewska@users.noreply.github.com>
2023-05-22 11:07:52 +0200

    Update list_from_another_list.py

--- list_from_another_list.py
+++ list_from_another_list.py
@@ -7,3 +7,18 @@
 #Given:
 list1 = [10, 20, 25, 30, 35]
 list2 = [40, 45, 60, 75, 90]
+#create a new list:
+list3 = []
+
+#print only odd numbers (list1):
+for num in list1:
+    if num % 2 == 1:
+        list3.append(num)
+        
+#print only even numbers (list2):
+for num in list2:
+    if num % 2 == 0:
+        list3.append(num)  
+        
+#print list3:
+print('result list:', list3)
